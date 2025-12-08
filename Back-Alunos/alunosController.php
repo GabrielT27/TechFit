7700a0f58af76bf958ec60df88feedf6db652fa6
GabrielT27 <gabrielmarquesterr@gmail.com>
2025-12-08 02:45:10 -0300

    CRUD e DAO dos alunos feito.

---
+++ Back-Alunos/alunosController.php
@@ -0,0 +1,55 @@
+<?php 
+
+require_once __DIR__ . '/alunosDAO.php';
+require_once __DIR__ . '/alunos.php';
+
+class AlunosController {
+
+    private $dao;
+
+
+    public function __construct() {
+        $this->dao = new AlunosDAO();
+    }
+
+    // READ - listar todos os alunos
+
+    public function ler() {
+        return $this->dao->lerAlunos();
+    }
+
+
+    // CREATE - criar novo aluno 
+
+    public function criar($nome_aluno, $sobrenome_aluno, $status, $plano, $email_aluno, $cpf) {
+
+        $aluno = new Aluno(null, $nome_aluno, $sobrenome_aluno, $status,  $plano, $email_aluno, $cpf);
+        $this->dao->criarAlunos($aluno);
+
+    }
+
+
+
+    // UPDATE - ATUALIZAR ALUNO EXISTE 
+
+    public function atualizar($id_aluno, $novonome_aluno, $novosobrenome_aluno, $novostatus, $novoplano, $novoemail_aluno, $novocpf) {
+
+        $this->dao->atualizarAlunos($id_aluno, $novonome_aluno, $novosobrenome_aluno, $novostatus, $novoplano, $novoemail_aluno, $novocpf);
+    }
+
+
+    // DELETE - EXCLUIR ALUNO 
+
+    public function excluir($id_aluno) {
+        return $this->dao->excluirAlunos($id_aluno);
+    }
+
+    // BUSCAR - BUSCAR ALUNOS POR NOMES
+
+    public function buscarPorNome($nome_aluno) {
+        return $this->dao->buscarPorNome($nome_aluno);
+    }
+}
+
+
+?>
