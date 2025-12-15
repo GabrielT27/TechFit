c76289af8e339f50dee349c9221cc119e7e6afb2
GabrielT27 <gabrielmarquesterr@gmail.com>
2025-12-15 14:58:20 -0300

    modal funcionários criado.

--- Back-Funcionarios/Modal/funcionarios.php
+++ Back-Funcionarios/Modal/funcionarios.php
@@ -59,6 +59,124 @@ class Funcionario{
 
 
 
+    public function getNomeFunc() {
+        return $this->nomeFunc;
+    }
+
+    public function setNomeFunc($nomeFunc) {
+        $this->nomeFunc = $nomeFunc;
+
+        return $this;
+    }
+
+
+
+    public function getCargo() {
+        return $this->cargo;
+    }
+
+
+    public function setCargo($cargo) {
+        $this->cargo = $cargo;
+
+        return $this;
+    }
+
+
+
+    public function getTurno() {
+        return $this->turno;
+    }
+
+
+    public function setTurno($turno) {
+        $this->turno = $turno;
+
+        return $this;
+    }
+
+
+    public function getEmailFunc() {
+        return $this->emailFunc;
+    }
+
+    public function setEmailFunc($emailFunc) {
+        $this->emailFunc=$emailFunc;
+
+        return $this;
+    }
+
+    public function getEnderecoFunc() {
+        return $this->enderecoFunc;
+    }
+
+    public function setEnderecoFunc($enderecoFunc) {
+        $this->enderecoFunc=$enderecoFunc;
+
+        return $this;
+    }
+
+    public function getCpfFunc() {
+        return $this->cpfFunc;
+    }
+
+    public function setCpfFunc($cpfFunc) {
+        $this->cpfFunc = $cpfFunc;
+        
+        return $this;
+    }
+
+    public function getTelFunc() {
+        return $this->telFunc;
+    }
+
+    public function setTelFunc($telFunc) {
+        $this->telFunc = $telFunc;
+
+        return $this;
+    }
+
+    public function getDtnFunc() {
+        return $this->dtnFunc;
+    }
+
+    public function setDtnFunc($dtnFunc) {
+        $this->dtnFunc = $dtnFunc;
+
+        return $this;
+    }
+
+    public function getDtAdm() {
+        return $this->dt_admFunc;
+}
+
+    public function setDtAdm($dt_admFunc) {
+        $this->dt_admFunc =$dt_admFunc;
+
+        return $this;
+    }
+
+    public function getSalario() {
+        return $this->salario;
+
+    }
+
+    public function setSalario($salario) {
+        $this->salario = $salario;
+
+        return $this;
+    }
+
+    public function getIdModalidade() {
+        return $this->id_modalidade;
+    }
+
+    public function setIdModalidade($id_modalidade) {
+        $this->id_modalidade=$id_modalidade;
+
+        return $this;
+
+    }
     
 
 }
